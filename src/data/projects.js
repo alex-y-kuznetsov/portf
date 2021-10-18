d1ebe4e994ee68236a98fff2d32d11301686c22d
Alex <alex.y.kuznetsov@gmail.com>
2021-10-18 19:57:44 +0500

    layout projects

---
+++ src/data/projects.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    id: 1,
+    name: 'Clash',
+    img: '',
+    subtitle: 'A simple fighting app in a fantasy world'
+  }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="home">
-    Home
+  <div class="home container">
+    <div class="projects">
+      <article class="project_item"
+               v-for="project in projects"
+               v-bind:key ="project.id">
+        <h1 class="project_title">{{ project.name }}</h1>
+        <h2 class="project_subtitle">{{ project.subtitle }}</h2>
+        <div class="project_img"></div>
+        <div class="project_description"></div>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
+import projects from '@/data/projects.js'
 
 export default {
   name: 'Home',
   components: {
+  },
+  data () {
+    return {
+      projects
+    }
   }
 }
 </script>
