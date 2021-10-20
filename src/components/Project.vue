54a2fe788df030e3ebabfbeeb4af05b7394a22b2
Alex <alex.y.kuznetsov@gmail.com>
2021-10-20 13:01:20 +0500

    project layout and previews, data restructured

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,14 +2,24 @@
   <article class="project_item">
     <h1 class="project_title">{{ currentProject.name }}</h1>
     <h2 class="project_subtitle">{{ currentProject.subtitle }}</h2>
-    <div class="project_img"
-         v-bind:style="{ backgroundImage: 'url(' + constants.PROJ_PATH + currentProject.img + ')' }">
-      <div class="project_links">
-        <a class="project_link" target="_blank" v-bind:href="currentProject.prodUrl">Production</a>
-        <a class="project_link" target="_blank" v-bind:href="currentProject.devUrl">Repository</a>
-      </div>
+    <div class="project_img_container">
+      <img class="project_img"
+           v-for="(projectImage, index) in currentProject.images"
+           v-bind:key="index"
+           v-bind:src="projectImage" />
     </div>
-    <div class="project_description"></div>
+    <div class="project_description">
+      <span class="project_description_subline">Features:</span>
+      <ul class="project_features">
+        <li class="project_features_item"
+            v-for="(projectFeature, index) in currentProject.features"
+            v-bind:key="index">{{ projectFeature }}</li>
+      </ul>
+    </div>
+    <div class="project_links">
+      <a class="project_link" target="_blank" v-bind:href="currentProject.prodUrl">Production</a>
+      <a class="project_link" target="_blank" v-bind:href="currentProject.devUrl">Repository</a>
+    </div>
   </article>
 </template>
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,23 +2,32 @@ export default [
   {
     id: 1,
     name: 'Clash',
-    img: '/img/project_previews/clash_preview.jpg',
     prodUrl: 'https://alex-y-kuznetsov.github.io/clash-build/',
     devUrl: 'https://github.com/alex-y-kuznetsov/clash',
     subtitle: 'A simple fighting app in a fantasy world',
+    images: [
+      '/img/project_previews/clash/clash_preview_1.jpg',
+      '/img/project_previews/clash/clash_preview_2.jpg',
+      '/img/project_previews/clash/clash_preview_3.jpg'
+    ],
     features: [
-      '',
-      '',
-      ''
+      'Vue store interaction between components',
+      'Character stats are affected by items',
+      'Transition between pages Vue via routing',
+      'Desktop and mobile versions'
     ]
   },
   {
     id: 2,
     name: 'Sleep',
-    img: '/img/project_previews/sleep_preview.jpg',
     prodUrl: 'https://alex-y-kuznetsov.github.io/sleep-build/',
     devUrl: 'https://github.com/alex-y-kuznetsov/sleep',
     subtitle: 'An app to keep track how many times a month my fiancée falls asleep on the couch',
+    images: [
+      '/img/project_previews/sleep/sleep_preview_1.jpg',
+      '/img/project_previews/sleep/sleep_preview_2.jpg',
+      '/img/project_previews/sleep/sleep_preview_3.jpg'
+    ],
     features: [
       '',
       '',
